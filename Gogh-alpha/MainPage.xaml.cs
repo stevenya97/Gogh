8c5b72ef86343d693a4c03a56e30e7dc2649b5bc
Steven Yang <stevenya@live.com>
2020-08-25 23:53:53 -0400

    first push

---
+++ Gogh-alpha/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Gogh_alpha
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            //MaximizeWindowOnLoad();
+
+            this.InitializeComponent();
+
+            void MaximizeWindowOnLoad()
+            {
+                var view = DisplayInformation.GetForCurrentView();
+
+                // Get the screen resolution (APIs available from 14393 onward).
+                var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+
+                // Calculate the screen size in effective pixels. 
+                // Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
+                var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
+                var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
+
+                ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+                ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            }
+
+
+        }
+
+        private void Control1_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            //ZoomSlider.Value = Control1.ZoomFactor;
+        }
+    }
+}
